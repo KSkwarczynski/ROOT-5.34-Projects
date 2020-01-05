3740361de29a9783cd79f51e20120053d1ee8431
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2020-01-05 16:52:13 +0100

    Add files via upload

---
+++ Highland2/Tutorial/AddingNewVariables/RecNuEnXsecMultiPi.cpp
@@ -0,0 +1,78 @@
+#include <fstream>
+#include <iostream>
+#include "TMath.h"
+
+void RecNuEnXsecMultiPi()
+{
+    gStyle->SetOptStat(0);
+    gStyle->SetPadColor(0);
+    gStyle->SetCanvasColor(0);
+
+    TString Directory ="/mnt/home/kskwarczynski/T2K/work/v11r31/highland2/antiNumuCC1piXsecAnalysis/v0r2/Linux-x86_64/";
+    
+    DrawingTools draw(Form("%sProd6tRun5XsecAntiNuMultiPiAccum5.root",Directory.Data()), false);
+    Experiment exp("nd280");
+    
+    DataSample* R5mc5 = new DataSample(Form("%sProd6tRun5XsecAntiNuMultiPiAccum5.root",Directory.Data()));
+    
+    SampleGroup run5("run5");
+    run5.AddMCSample("R5mc5", R5mc5);
+
+    exp.AddSampleGroup("run5",run5);
+    
+    DataSample* R6mc5 = new DataSample(Form("%sProd6tRun6XsecAntiNuMultiPiAccum5.root",Directory.Data()));
+    
+    SampleGroup run6("run6");
+    run6.AddMCSample("R6mc5", R6mc5);
+    
+    exp.AddSampleGroup("run6",run6);
+    
+    DataSample* R7mc5 = new DataSample(Form("%sProd6tRun7XsecAntiNuMultiPiAccum5.root",Directory.Data()));
+    
+    SampleGroup run7("run7");
+    run7.AddMCSample("R7mc5", R7mc5);
+    
+    exp.AddSampleGroup("run7",run7);
+    
+    const int PlotNumber=5;
+    TString PathName="/mnt/home/kskwarczynski/T2K/PlotOutputs/MyVariableNuEnergy/";
+    TString FileNames[PlotNumber]={"XsecMultiPiRecEnTopology.pdf", "XsecMultiPiRecEnParticle.pdf", "XsecMultiPiRecEnReaction.pdf", "XsecMultiPiRecVsTrue.pdf", "XsecMultiPiRecVsTrueTopTrue.pdf" };
+    TString PrintName[PlotNumber];
+    
+    for(int i=0; i<PlotNumber; i++)
+    {
+        PrintName[i]=PathName+FileNames[i];
+    }
+    c1 = new TCanvas("c1"," ",10,10,800,600);
+   
+    draw.SetTitle("accum[0][0]>8 #bar{#nu} rec energy");
+    draw.SetTitleX("#bar{#nu} reconstructed energy [MeV]");
+    draw.SetLegendPos("tr");
+    draw.Draw(exp,"nuEnergy",50,0., 6000.,"topology","accum_level[0][0]>8");
+    c1->Print(PrintName[0]);
+    
+    draw.SetTitle("accum[0][0]>8 #bar{#nu} rec energy");
+    draw.SetTitleX("#bar{#nu} reconstructed energy [MeV]");
+    draw.SetLegendPos("tr");
+    draw.Draw(exp,"nuEnergy",50,0., 6000.,"particle","accum_level[0][0]>8");
+    c1->Print(PrintName[1]);
+    
+    draw.SetTitle("accum[0][0]>8 #bar{#nu} rec energy");
+    draw.SetTitleX("#bar{#nu} reconstructed energy [MeV]");
+    draw.SetLegendPos("tr");
+    draw.Draw(exp,"nuEnergy",50,0., 6000.,"reaction","accum_level[0][0]>8");
+    c1->Print(PrintName[2]);
+    
+    draw.SetTitle("#bar{#nu} rec energy vs true energy accum[0][0]>8");
+    draw.SetTitleX("#bar{#nu} reconstructed energy");
+    draw.SetTitleY("#bar{#nu} true energy");
+    draw.Draw(exp,"nu_trueE:nuEnergy", 50,0., 6000., 50,0., 6000., "all","accum_level[0][0]>8" ,"colz");
+    c1->Print(PrintName[3]);
+    
+    draw.SetTitle("#bar{#nu} rec energy vs true energy accum[0][0]>8 true topology");
+    draw.SetTitleX("#bar{#nu} reconstructed energy");
+    draw.SetTitleY("#bar{#nu} true energy");
+    draw.Draw(exp,"nu_trueE:nuEnergy", 50,0., 6000., 50,0., 6000., "all","accum_level[0][0]>8 && topology==1" ,"colz");
+    c1->Print(PrintName[4]);
+    
+}
