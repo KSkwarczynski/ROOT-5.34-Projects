32c1e2afe505b23eb23b631d3c9bbe3dc5c21cf2
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2020-02-03 14:17:58 +0100

    Add files via upload

--- Highland2/Tutorial/ParticleGun/ParticleGun.cpp
+++ Highland2/Tutorial/ParticleGun/ParticleGun.cpp
@@ -16,8 +16,14 @@ void ParticleGun()
     TString Particle[6]={"mu-", "mu+", "anti_proton", "proton", "pi-", "pi+"};
     
     TString PathName="/mnt/home/kskwarczynski/T2K/PlotOutputs/ParticleGunDistance/";
-    TCanvas* c1[30][6];
+    TCanvas* c1[6][30][6];
     
+    
+    TString Cuts[5]={"accum_level[0][0]>1", "accum_level[0][0]>1 && selmu_endpos[2]>2500", "accum_level[0][0]>1 && selmu_endpos[2]<2500", "accum_level[0][0]>1 && selmu_endpos[2]>2500", "accum_level[0][0]>1 && selmu_endpos[2]<2500"};
+
+    TString Variable[5]={"selmu_endpos[2]", "selmu_endpos[0]", "selmu_endpos[0]", "selmu_endpos[1]", "selmu_endpos[1]"};
+    
+    TString OutputName[5]={"EndPos-Z", "EndPos-X-ifZ>2500", "EndPos-X-ifZ<2500", "EndPos-Y-ifZ>2500", "EndPos-Y-ifZ<2500"};
     for(int ig=200; ig<2001; ig=ig+100)
     {
         for(int ik=0; ik<6; ik++)
@@ -29,15 +35,31 @@ void ParticleGun()
                 cout<<"opening "<< Form("%sParticleGun-%s-%i-00-Accum0.root",Directory.Data(), Particle[ik].Data(), ig/100) <<endl;
                 
                 DataSample* ParticleGunSample = new DataSample( Form("%sParticleGun-%s-%i-00-Accum0.root",Directory.Data(), Particle[ik].Data(), ig/100) );
-                c1[ig/100][ik] = new TCanvas( Form("c1-%i-%i",ig, ik), Form("c1-%i-%i",ig, ik), 10,10,800,600);
-   
-                draw.SetTitle( Form("EndPosEnergy-%s-E%i.pdf", Particle[ik].Data(), ig) );
-                draw.SetTitleX("endpos [cm]");
-                draw.SetLegendPos("tr");
-        
-                draw.Draw(*ParticleGunSample,"selmu_endpos[2]", 40, 0., 3500,"topology","accum_level[0][0]>1");
-                c1[ig/100][ik]->Print( Form("%sEndPosEnergy-%s-E%i.pdf", PathName.Data(), Particle[ik].Data(), ig) );
-                delete c1[ig/100][ik];
+                for(int ic=0; ic<5; ic++)
+                {
+                    c1[ic][ig/100][ik] = new TCanvas( Form("c1-%i-%i-%i",ic , ig, ik), Form("c1-%i-%i-%i", ic, ig, ik), 10,10,800,600);
+                    draw.SetTitle( Form("%s-%s-E%i", OutputName[ic].Data(), Particle[ik].Data(), ig) );
+                    draw.SetTitleX("endpos [cm]");
+                    draw.SetLegendPos("tl");
+                    if(ic>0) 
+                    {
+                        draw.SetLegendPos("tr");
+                    }
+                    draw.Draw(*ParticleGunSample, Form("%s", Variable[ic].Data() ), 40, 0., 3500,"topology", Form("%s", Cuts[ic].Data() ));
+                    c1[ic][ig/100][ik]->Print( Form("%s%s-%s-E%i.pdf", PathName.Data(), OutputName[ic].Data(), Particle[ik].Data(), ig) );
+                    delete c1[ic][ig/100][ik];
+                
+                }
+                
+                c1[5][ig/100][ik] = new TCanvas( Form("c1-5-%i-%i",ic , ig, ik), Form("c1-5-%i-%i", ig, ik), 10,10,800,600);
+                draw.SetTitle( Form("CosTheta-%s-E%i", Particle[ik].Data(), ig) );
+                draw.SetTitleX("CosTheta");
+                draw.SetLegendPos("tl");
+                    
+                draw.Draw(*ParticleGunSample,"selmu_costheta", 40,0.0,1.0 ,"topology","accum_level[0][0]>1");
+                c1[5][ig/100][ik]->Print( Form("%sCosTheta-%s-E%i.pdf", PathName.Data(), Particle[ik].Data(), ig) );
+                delete c1[5][ig/100][ik];
+                
                 delete ParticleGunSample;
             }
         }
