9ed21ffd12ecac23b6c1deb38e06a0ee53bdbe87
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2020-07-17 11:16:10 +0200

    Update HybridsSystematic.C

--- Highland2/Systematic/HybridsSystematic.C
+++ Highland2/Systematic/HybridsSystematic.C
@@ -30,12 +30,12 @@ void HybridsSystematic(TString Path, TString FileName)
 
     exp.GetMCSample("run" , "magnet")->SetCurrentTree(syst_name);
     cout<<"CC-0Pi below"<<endl;
-    draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[0][0]>7 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS");
+    draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[][0]>7 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS");
     cout<<"CC-1Pi below"<<endl;
-    draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[0][1]>7 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS");
+    draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[][1]>7 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS");
     cout<<"CC-Other below"<<endl;
-    draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[0][2]>7 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS");
-    //draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[0][2]>6 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS"); //WARNING for neutrino
+    draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[][2]>7 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS");
+    //draw->DrawRelativeErrors(exp,"0.",1,-1,1,"accum_level[][2]>6 && weight_syst_total<10 && weight_syst_total > 0", "", "SYS"); //WARNING for neutrino
     
         
     cout.rdbuf(backup);// restore cout's original streambuf
@@ -44,18 +44,18 @@ void HybridsSystematic(TString Path, TString FileName)
     TFile *fileout = new TFile(Form("%s_plots.root",FileName.Data()),"RECREATE");
     
     TCanvas* CC0Pi = new TCanvas("CC0Pi","CC0Pi",0, 0, 800,630);
-    draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[0][0]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS");
+    draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[][0]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS");
     CC0Pi->Write();
     delete CC0Pi;
     
     TCanvas* CC1Pi = new TCanvas("CC1Pi","CC1Pi",0, 0, 800,630);
-    draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[0][1]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS");
+    draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[][1]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS");
     CC1Pi->Write();
     delete CC1Pi;
     
     TCanvas* CCOther = new TCanvas("CCOther","CCOther",0, 0, 800,630);
-    //draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[0][2]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS");
-    draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[0][2]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS"); //WARNING for neutrino
+    //draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[][2]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS");
+    draw->DrawRelativeErrors(exp,"selmu_mom",20, 0., 5000., "accum_level[][2]>7 && weight_syst_total<10 && weight_syst_total > 0" , "", "SYS"); //WARNING for neutrino
     CCOther->Write();
     delete CCOther;
     
