84efd401a2b5713245d85e76cd871d4b8220081c
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2021-07-22 11:33:11 +0200

    Add files via upload

---
+++ Plots/invert.cpp
@@ -0,0 +1,177 @@
+void invert(std::string filename) {
+   
+  TCanvas *canv = new TCanvas("canv", "canv", 1024, 1024);
+  SetT2Kstyl();
+  gPad->SetGrid();
+  gStyle->SetPalette(51); 
+  gStyle->SetPaintTextFormat("4.1f"); //Precision of value in matrix element
+  
+  TFile *f = new TFile(filename.c_str());
+  TMatrixDSym *cov = (TMatrixDSym*)f->Get("xsec_cov");
+  TString canvname = f->GetName();
+  canvname.ReplaceAll(".root","");
+  canvname+=".pdf";
+  TMatrixDSym *invcov = (TMatrixDSym*)cov->Clone("invcov");
+  invcov->Invert();
+
+  canv->SetRightMargin(canv->GetRightMargin()*1.5);
+  canv->Print(canvname+"[");
+  cov->Draw("colz text");
+  canv->Print(canvname);
+  invcov->Draw("colz text");
+  canv->Print(canvname);
+  canv->Print(canvname+"]");
+}
+
+
+void SetT2Kstyl()
+{
+     // -- WhichStyle --
+    // 1 = presentation large fonts
+    // 2 = presentation small fonts
+    // 3 = publication/paper
+    Int_t WhichStyle = 3;
+    
+    Int_t FontStyle = 22;
+    Float_t FontSizeLabel = 0.035;
+    Float_t FontSizeTitle = 0.05;
+    Float_t YOffsetTitle = 1.3;
+
+  switch(WhichStyle) 
+  {
+    case 1:
+        FontStyle = 42;
+        FontSizeLabel = 0.05;
+        FontSizeTitle = 0.065;
+        YOffsetTitle = 1.19;
+        break;
+    case 2:
+        FontStyle = 42;
+        FontSizeLabel = 0.035;
+        FontSizeTitle = 0.05;
+        YOffsetTitle = 1.6;
+        break;
+    case 3:
+        FontStyle = 132;
+        FontSizeLabel = 0.035;
+        FontSizeTitle = 0.045;
+        YOffsetTitle = 1.6;
+        break;
+    }
+    // use plain black on white colors
+    gStyle->SetFrameBorderMode(0);
+    gStyle->SetCanvasBorderMode(0);
+    gStyle->SetPadBorderMode(0);
+    gStyle->SetCanvasBorderSize(0);
+    gStyle->SetFrameBorderSize(0);
+    gStyle->SetDrawBorder(0);
+    gStyle->SetTitleBorderSize(2);
+    gStyle->SetStatBorderSize(2);
+
+    gStyle->SetStatX(0.89);
+    gStyle->SetStatY(0.89);
+    
+    gStyle->SetPadColor(0);
+    gStyle->SetCanvasColor(0);
+    gStyle->SetStatColor(0);
+    gStyle->SetFillColor(0);
+
+    gStyle->SetEndErrorSize(4);
+    gStyle->SetStripDecimals(kFALSE);
+
+    //gStyle->SetLegendBorderSize(0); //This option dsables legends borders
+    gStyle->SetLegendFont(FontStyle);
+
+    // set the paper & margin sizes
+    gStyle->SetPaperSize(20, 26);
+    gStyle->SetPadTopMargin(0.1);
+    gStyle->SetPadBottomMargin(0.12);
+    gStyle->SetPadRightMargin(0.12); // 0.075 -> 0.13 for colz option
+    gStyle->SetPadLeftMargin(0.12);//to include both large/small font options
+
+    // Fonts, sizes, offsets
+    gStyle->SetTextFont(FontStyle);
+    gStyle->SetTextSize(FontSizeTitle);
+
+    gStyle->SetLabelFont(FontStyle, "x");
+    gStyle->SetLabelFont(FontStyle, "y");
+    gStyle->SetLabelFont(FontStyle, "z");
+    gStyle->SetLabelFont(FontStyle, "t");
+    gStyle->SetLabelSize(FontSizeLabel, "x");
+    gStyle->SetLabelSize(FontSizeLabel, "y");
+    gStyle->SetLabelSize(0.030, "z");
+    gStyle->SetLabelOffset(0.008, "x");
+    gStyle->SetLabelOffset(0.008, "y");
+    gStyle->SetLabelOffset(0.006, "z");
+
+    gStyle->SetTitleFont(FontStyle, "x");
+    gStyle->SetTitleFont(FontStyle, "y");
+    gStyle->SetTitleFont(FontStyle, "z");
+    gStyle->SetTitleFont(FontStyle, "t");
+    gStyle->SetTitleSize(FontSizeTitle, "y");
+    gStyle->SetTitleSize(FontSizeTitle, "x");
+    gStyle->SetTitleSize(FontSizeTitle, "z");
+    gStyle->SetTitleSize(FontSizeTitle, "t");
+    gStyle->SetTitleOffset(1.1, "x");
+    gStyle->SetTitleOffset(1.1, "y");
+    gStyle->SetTitleOffset(1.1, "z");
+
+    gStyle->SetTitleStyle(0);
+    gStyle->SetTitleFontSize(0.045);//0.08
+    gStyle->SetTitleFont(FontStyle, "pad");
+    gStyle->SetTitleX(0.6);
+    gStyle->SetTitleY(0.96);
+    //gStyle->SetTitleW(0.8f);
+
+    // use bold lines and markers
+    gStyle->SetMarkerStyle(20); //FIXME Coment this for FGD2
+    gStyle->SetHistLineWidth( Width_t(2.5) );
+    gStyle->SetLineStyleString(2, "[12 12]"); // postscript dashes
+
+    // get rid of X error bars and y error bar caps
+    gStyle->SetErrorX(0.001);
+
+    // do not display any of the standard histogram decorations
+    //gStyle->SetOptTitle(0); //Set 0 to disable histogram tittle
+    gStyle->SetOptStat(0); //Set 0 to disable statystic box
+    //gStyle->SetOptFit(0);
+
+    // put tick marks on top and RHS of plots
+    gStyle->SetPadTickX(0);
+    gStyle->SetPadTickY(0);
+
+    // -- color --
+    // functions blue
+    //gStyle->SetFuncColor(600-4);
+    gStyle->SetFuncColor(2);
+    gStyle->SetFuncWidth(2);
+
+    gStyle->SetFillColor(1); // make color fillings (not white)
+    // - color setup for 2D -
+    // - "cold"/ blue-ish -
+    Double_t red[]   = { 0.00, 0.00, 0.00 };
+    Double_t green[] = { 1.00, 0.00, 0.00 };
+    Double_t blue[]  = { 1.00, 1.00, 0.25 };
+    // - "warm" red-ish colors -
+    //  Double_t red[]   = {1.00, 1.00, 0.25 };
+    //  Double_t green[] = {1.00, 0.00, 0.00 };
+    //  Double_t blue[]  = {0.00, 0.00, 0.00 };
+
+    Double_t stops[] = { 0.25, 0.75, 1.00 };
+    const Int_t NRGBs = 3;
+    const Int_t NCont = 500;
+
+    TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+    gStyle->SetNumberContours(NCont);
+
+    // - Rainbow -
+    gStyle->SetPalette(1);  // use the rainbow color set
+
+    // -- axis --
+    gStyle->SetStripDecimals(kFALSE); // don't do 1.0 -> 1
+    //  TGaxis::SetMaxDigits(3); // doesn't have an effect
+    // no supressed zeroes!
+    gStyle->SetHistMinimumZero(kTRUE);       
+    
+    gROOT->ForceStyle();    
+}
