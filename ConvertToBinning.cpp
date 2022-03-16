0f7d74d12adb9e99264220aff400eb414e5b5fc9
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2022-03-16 19:32:02 +0100

    Add files via upload

---
+++ ConvertToBinning.cpp
@@ -0,0 +1,77 @@
+#include "TH1D.h"
+#include "TH2Poly.h"
+#include "TPad.h"
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+#include "TLegend.h"
+#include "TFile.h"
+#include "TKey.h"
+#include "THStack.h"
+#include "TROOT.h"
+#include "TGaxis.h"
+#include "TRandom3.h"
+#include "TRandom3.h"
+#include "omp.h"
+#include <iostream>
+
+// g++ `root-config --cflags`-std=c++11 -fopenmp -g -o  ConvertToBinning ConvertToBinning.cpp -I`root-config --incdir` `root-config --glibs --libs`
+
+void Binning(std::string filename, std::string binPoly);
+    
+int main(int argc, char *argv[]) 
+{
+    std::string filename = argv[1];
+        std::string binPoly = argv[2];
+    Binning(filename, binPoly );
+    return 0;
+}
+void Binning(std::string filename, std::string binPoly) {
+
+   TFile *f1 = TFile::Open(filename.c_str());
+   TFile *Binning = TFile::Open(binPoly.c_str());
+   
+   std::string dupa = filename + binPoly;
+   TFile *OutputDupa = TFile::Open(dupa.c_str(), "recreate");
+   //Get all entries in input file
+   std::string name;
+   TIter next(f1->GetListOfKeys());
+   TKey *key;
+   
+   //Main loop to get PriorPredicvitve Disribution
+   while ((key = (TKey*)next())) 
+   {
+      std::string classname = std::string(key->GetClassName());  
+      if (classname != "TH2Poly") continue;
+      name = std::string(key->GetName()); 
+      TH2Poly *poly = (TH2Poly*)key->ReadObj();
+      TIter nextsub(gDirectory->GetListOfKeys());
+       
+      if (name.find("_W2Hist") != std::string::npos) continue;
+
+        std::string pupa = "MC_" + name;
+        
+        TH2Poly *FluctHist = (TH2Poly*)(Binning->Get(name.c_str() ));
+        std::cout<<pupa<<std::endl;
+        double integral = 0;
+        //#pragma omp parallel  for reduction(+:integral)
+        for (int i = 1; i < poly->GetNumberOfBins()+1; ++i) 
+        {
+            //get bin and its edges
+            TH2PolyBin* bin = (TH2PolyBin*)((TH2Poly*)poly)->GetBins()->At(i-1)->Clone();
+            double ymas = bin->GetYMax() - 0.005;
+            double xup = bin->GetXMax() - 25;
+            FluctHist->SetBinContent(FluctHist->FindBin(xup, ymas), FluctHist->GetBinContent(i) + bin->GetContent());
+            integral += bin->GetContent();
+            delete bin;
+        }
+        std::cout<<integral<<std::endl;
+        OutputDupa->cd();
+        FluctHist->Write(pupa.c_str());
+          
+   }
+
+      
+  OutputDupa->Close();    
+                    
+}
+
