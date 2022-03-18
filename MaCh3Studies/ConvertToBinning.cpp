c65bd5ebe2a66cfb7c21580145beeeeda2fc1f1c
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2022-03-18 16:59:26 +0100

    Add files via upload

--- MaCh3Studies/ConvertToBinning.cpp
+++ MaCh3Studies/ConvertToBinning.cpp
@@ -17,12 +17,13 @@
 // g++ `root-config --cflags`-std=c++11 -fopenmp -g -o  ConvertToBinning ConvertToBinning.cpp -I`root-config --incdir` `root-config --glibs --libs`
 
 void Binning(std::string filename, std::string binPoly);
-    
+  double NoOverflowIntegral(TH2Poly* poly);
+  
 int main(int argc, char *argv[]) 
 {
     std::string filename = argv[1];
         std::string binPoly = argv[2];
-    Binning(filename, binPoly );
+    Binning(filename, binPoly);
     return 0;
 }
 void Binning(std::string filename, std::string binPoly) {
@@ -30,7 +31,7 @@ void Binning(std::string filename, std::string binPoly) {
    TFile *f1 = TFile::Open(filename.c_str());
    TFile *Binning = TFile::Open(binPoly.c_str());
    
-   std::string dupa = filename + binPoly;
+   std::string dupa = filename + binPoly +".root";
    TFile *OutputDupa = TFile::Open(dupa.c_str(), "recreate");
    //Get all entries in input file
    std::string name;
@@ -51,6 +52,7 @@ void Binning(std::string filename, std::string binPoly) {
         std::string pupa = "MC_" + name;
         
         TH2Poly *FluctHist = (TH2Poly*)(Binning->Get(name.c_str() ));
+        FluctHist->Fill(0.0, 0.0, 0.0);
         std::cout<<pupa<<std::endl;
         double integral = 0;
         //#pragma omp parallel  for reduction(+:integral)
@@ -58,20 +60,39 @@ void Binning(std::string filename, std::string binPoly) {
         {
             //get bin and its edges
             TH2PolyBin* bin = (TH2PolyBin*)((TH2Poly*)poly)->GetBins()->At(i-1)->Clone();
-            double ymas = bin->GetYMax() - 0.005;
-            double xup = bin->GetXMax() - 25;
-            FluctHist->SetBinContent(FluctHist->FindBin(xup, ymas), FluctHist->GetBinContent(i) + bin->GetContent());
-            integral += bin->GetContent();
+            double ymas = bin->GetYMax() - 0.001;
+            double xup = bin->GetXMax() - 10;
+            double BinCont = bin->GetContent();
+            integral += BinCont;
+            int NewBin = FluctHist->FindBin(xup, ymas);
+            BinCont += FluctHist->GetBinContent(NewBin);
+            FluctHist->SetBinContent(NewBin, BinCont);
+
             delete bin;
         }
-        std::cout<<integral<<std::endl;
+         std::cout<<"old  "<<integral<<std::endl;
+         std::cout<<"new "<<NoOverflowIntegral(FluctHist)<<std::endl;
+        
         OutputDupa->cd();
         FluctHist->Write(pupa.c_str());
-          
    }
 
-      
   OutputDupa->Close();    
                     
 }
 
+
+
+double NoOverflowIntegral(TH2Poly* poly) {
+  // **************************************************
+
+  double integral=0;
+
+  for(int i=1; i<poly->GetNumberOfBins()+1; i++)
+    {
+      integral += poly->GetBinContent(i);
+    }
+
+  return integral;
+
+} // end function
