a92a5c6bbbf10be310d495c9610574b80fea1fb7
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2022-07-22 11:49:59 +0200

    Add files via upload

--- ProtonSelection/Highland2/DataMCproton.cxx
+++ ProtonSelection/Highland2/DataMCproton.cxx
@@ -29,171 +29,168 @@ void DataMCproton()
     //gStyle->SetCanvasColor(0);
 
     //TODO hardocoded value
-    DrawingTools draw("/mnt/home/share/t2k/kskwarczynski/BANFF/NewSelection/Resolution/DataResolution.root");
+    DrawingTools draw("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run2aMC.root");
+
+    Experiment exp("nd280");
 
 
-    DataSample data("/mnt/home/share/t2k/kskwarczynski/BANFF/NewSelection/Resolution/DataResolution.root");
-    DataSample mc("/mnt/home/share/t2k/kskwarczynski/BANFF/NewSelection/Resolution/ResolutionMC.root");
-    
+    DataSample* R2aMc = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run2aMC.root");
+    DataSample* R2aMC_sand = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/SandFHC.root");
+    DataSample* R2aData = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/DataRun2a.root");
         
-    double POTData = 115.31e+19;
-    double POTMC = 1.93389e+22;
-    double POT_Weight = POTData/POTMC;
-    
-    
-    const int    FHCNumuCC0Pi_Mom_NBin        = 28;
-    const double FHCNumuCC0Pi_Mom_Bin[29]     = {0.   , 200. , 300. , 400. , 450. , 500. , 550. , 600. ,650. , 700. , 750. , 800. , 850. , 900. , 950. , 1000., 1050., 1100., 1200., 1300., 1400., 1500., 1600., 1700., 1800., 2000., 2500., 3000., 5000.};
-                                         
-    const int    FHCNumuCC0Pi_0p_Mom_NBin        = 20;
-    const double FHCNumuCC0Pi_0p_Mom_Bin[21]     = {0., 200., 300., 350., 400., 450., 500., 550., 600., 650., 700., 800., 900., 1000., 1250., 1500., 2000., 2500., 3000., 3500., 5000.};                                    
-                                     
-    const int    FHCNumuCC0Pi_Np_Mom_NBin        = 17;
-    const double FHCNumuCC0Pi_Np_Mom_Bin[18]     = {0., 300., 400., 500., 550., 600., 650., 700., 800., 900., 1000., 1250., 1500., 1750., 2000., 2750., 3500., 5000.};
-    
-    
-    const int    FHCNumuCC0Pi_Cos_NBin = 28;
-    const double FHCNumuCC0Pi_Cos_Bin[29] = { 0.5, 0.6, 0.7, 0.76, 0.78, 0.8, 0.83, 0.85, 0.88, 0.89, 0.9, 0.91, 0.92, 0.925, 0.93, 0.935, 0.94, 0.945, 0.95, 0.955, 0.96, 0.965, 0.97, 0.975, 0.98, 0.985, 0.99, 0.995, 1.};
-  
-  
-    const int    FHCNumuCC0Pi_0p_Cos_NBin = 13;
-    const double FHCNumuCC0Pi_0p_Cos_Bin[14] = {0.5, 0.6, 0.7, 0.80, 0.85, 0.88, 0.9, 0.92, 0.95, 0.96, 0.98, 0.99, 0.995, 1.0};
-    
-    const   int    FHCNumuCC0Pi_Np_Cos_NBin = 11;
-    const  double FHCNumuCC0Pi_Np_Cos_Bin[12] = {0.55, 0.65, 0.75, 0.8, 0.88, 0.9, 0.92, 0.95, 0.96, 0.975, 0.985, 1.0};
+    SampleGroup run2a("run2a");
+    run2a.AddDataSample(R2aData);
+    run2a.AddMCSample("R2aMc", R2aMc);
+    run2a.AddMCSample("R2aMC_sand", R2aMC_sand);
 
     
+    exp.AddSampleGroup("run2a",run2a);
+
+    DataSample* R2wMc = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run2wMC.root");
+    DataSample* R2wMC_sand = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/SandFHC.root");
+    DataSample* R2wData = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/DataRun2w.root");
+        
+    SampleGroup run2w("run2w");
+    run2w.AddDataSample(R2wData);
+    run2w.AddMCSample("R2wMc", R2wMc);
+    run2w.AddMCSample("R2wMC_sand", R2wMC_sand);
+
     
+    exp.AddSampleGroup("run2w",run2w);
+    
+    DataSample* R3Mc = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run3MC.root");
+    DataSample* R3MC_sand = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/SandFHC.root");
+    DataSample* R3Data = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/DataRun3.root");
+        
+    SampleGroup run3("run3");
+    run3.AddDataSample(R3Data);
+    run3.AddMCSample("R3Mc", R3Mc);
+    run3.AddMCSample("R3MC_sand", R3MC_sand);
+
+    
+    exp.AddSampleGroup("run3",run3);
+ 
+    DataSample* R4aMc = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run4aMC.root");
+    DataSample* R4aMC_sand = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/SandFHC.root");
+    DataSample* R4aData = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/DataRun4a.root");
+        
+    SampleGroup run4a("run4a");
+    run4a.AddDataSample(R4aData);
+    run4a.AddMCSample("R4aMc", R4aMc);
+    run4a.AddMCSample("R4aMC_sand", R4aMC_sand);
+
+    
+    exp.AddSampleGroup("run4a",run4a);
+    
+    DataSample* R4wMc = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run4wMC.root");
+    DataSample* R4wMC_sand = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/SandFHC.root");
+    DataSample* R4wData = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/DataRun4w.root");
+        
+    
+    SampleGroup run4w("run4w");
+    run4w.AddDataSample(R4wData);
+    run4w.AddMCSample("R4aMc", R4aMc);
+    run4w.AddMCSample("R4wMC_sand", R4wMC_sand);
+    
+    exp.AddSampleGroup("run4w",run4w);
+    
+    DataSample* R8aMc = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run8aMC.root");
+    DataSample* R8aMC_sand = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/SandFHC.root");
+    DataSample* R8aData = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/DataRun8a.root");
+        
+    SampleGroup run8a("run8a");
+    run8a.AddDataSample(R8aData);
+    run8a.AddMCSample("R8aMc", R8aMc);
+    run8a.AddMCSample("R8aMC_sand", R8aMC_sand);
+
+    
+    exp.AddSampleGroup("run8a",run8a);
+    
+    DataSample* R8wMc = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/Run8wMC.root");
+    DataSample* R8wMC_sand = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/SandFHC.root");
+    DataSample* R8wData = new DataSample("/mnt/home/share/t2k/kskwarczynski/HighlandOutput/Output/DataRun8w.root");
+        
+    SampleGroup run8w("run8w");
+    run8w.AddDataSample(R8wData);
+    run8w.AddMCSample("R8wMc", R8aMc);
+    run8w.AddMCSample("R8wMC_sand", R8wMC_sand);
+    
+    exp.AddSampleGroup("run8w",run8w);
     TCanvas *c1 = new TCanvas("c1"," ", 0, 0, 800,630);
-    c1->Print("ProtonSelecPlots.pdf[", "pdf");
-    
-    draw.SetLegendPos("tr");
-    draw.SetTitle("CC0Pi FGD1");
-    draw.SetTitleX("Muon candidate momentum [MeV/c]");
-    draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_Mom_NBin, FHCNumuCC0Pi_Mom_Bin, "reaction", "accum_level[0][0][0]>7");    
-    //draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_Mom_NBin, FHCNumuCC0Pi_Mom_Bin, "reaction", "accum_level[0][0][0]>7", "", "", false, POT_Weight, false);    
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    draw.SetLegendPos("tr");
-    draw.SetTitle("CC0Pi-0p FGD1");
-    draw.SetTitleX("Muon candidate momentum [MeV/c]");
-    draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_0p_Mom_NBin, FHCNumuCC0Pi_0p_Mom_Bin, "reaction", "accum_level[0][0][0]>8"); 
-    //draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_0p_Mom_NBin, FHCNumuCC0Pi_0p_Mom_Bin, "reaction", "accum_level[0][0][0]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    draw.SetLegendPos("tr");
-    draw.SetTitle("CC0Pi-Np FGD1");
-    draw.SetTitleX("Muon candidate momentum [MeV/c]");
-    draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_Np_Mom_NBin, FHCNumuCC0Pi_Np_Mom_Bin, "reaction", "accum_level[0][0][1]>8"); 
-    //draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_Np_Mom_NBin, FHCNumuCC0Pi_Np_Mom_Bin, "reaction", "accum_level[0][0][1]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    draw.SetLegendPos("tr");
-    draw.SetTitle("CC0Pi-0p FGD2");
-    draw.SetTitleX("Muon candidate momentum [MeV/c]");
-    draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_0p_Mom_NBin, FHCNumuCC0Pi_0p_Mom_Bin, "fgd2reaction", "accum_level[0][1][0]>8");
-    //draw.Draw(data, mc, "selmu_mom", FHCNumuCC0Pi_0p_Mom_NBin, FHCNumuCC0Pi_0p_Mom_Bin, "fgd2reaction", "accum_level[0][1][0]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    draw.SetLegendPos("tr");
-    draw.SetTitle("CC0Pi-Np FGD2");
-    draw.SetTitleX("Muon candidate momentum [MeV/c]");
-    draw.Draw(data, mc, "selmu_mom",FHCNumuCC0Pi_Np_Mom_NBin, FHCNumuCC0Pi_Np_Mom_Bin, "fgd2reaction", "accum_level[0][1][1]>8"); 
-    //draw.Draw(data, mc, "selmu_mom",FHCNumuCC0Pi_Np_Mom_NBin, FHCNumuCC0Pi_Np_Mom_Bin, "fgd2reaction", "accum_level[0][1][1]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-
+    c1->Print("ProtonSelecPlotsNew.pdf[", "pdf");
     
 
+    draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-0#gamma FGD1");
+    draw.SetTitleX("Number of reconstructer TPC protons");
+    draw.Draw(exp, "nProtonsTPC", 5., 0., 5., "reaction", "accum_level[0][0][2]>7");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+    draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-0#gamma FGD1");
+    draw.SetTitleX("Number of reconstructer FGD protons");
+    draw.Draw(exp, "nProtonsFGD", 5., 0., 5., "reaction", "accum_level[0][0][2]>7");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+    draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-0#gamma FGD2");
+    draw.SetTitleX("Number of reconstructer TPC protons");
+    draw.Draw(exp, "nProtonsTPC", 5., 0., 5., "fgd2reaction", "accum_level[0][1][2]>7");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+    draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-0#gamma FGD2");
+    draw.SetTitleX("Number of reconstructer FGD protons");
+    draw.Draw(exp, "nProtonsFGD", 5., 0., 5., "fgd2reaction", "accum_level[0][1][2]>7");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+    
+    
+    draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-Np-0#gamma FGD1");
+    draw.SetTitleX("reconstructed TPC proton momentum");
+    draw.Draw(exp, "sel_prot_mom_vertex",20,0., 2000, "reaction", "accum_level[0][0][2]>8 & nProtonsFGD == 0 && nProtonsTPC==1");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+    draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-Np-0#gamma FGD1");
+    draw.SetTitleX("reconstructed FGD proton momentum");
+    draw.Draw(exp, "sel_prot_mom_vertex",20,0., 2000, "reaction", "accum_level[0][0][2]>8 & nProtonsFGD == 1 && nProtonsTPC==0");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+    
+        draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-Np-0#gamma FGD2");
+    draw.SetTitleX("reconstructed TPC proton momentum");
+    draw.Draw(exp, "sel_prot_mom_vertex",20,0., 2000, "fgd2reaction", "accum_level[0][1][2]>8 & nProtonsFGD == 0 && nProtonsTPC==1");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+        draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-Np-0#gamma FGD2");
+    draw.SetTitleX("reconstructed FGD proton momentum");
+    draw.Draw(exp, "sel_prot_mom_vertex",20,0., 2000, "fgd2reaction", "accum_level[0][1][2]>8 & nProtonsFGD == 1 && nProtonsTPC==0");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+    
+    
+    double Blarb[13] = { 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.90, 0.92, 0.94, 0.96, 0.98, 0.99, 1.}
+    draw.SetLegendPos("tr");
+    draw.SetTitle("CC0#pi-Np-0#gamma FGD1");
+    draw.SetTitleX("Proton candidate momentum [MeV/c]");
+    draw.Draw(exp, "sel_prot_mom_vertex[0]", 20., 0., 1400., "reaction", "accum_level[0][0][2]>8");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+
     draw.SetLegendPos("tl");
-    draw.SetTitle("CC0Pi FGD1");
-    draw.SetTitleX("#mu^{-} candidate cos#theta");
-    draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_Cos_NBin, FHCNumuCC0Pi_Cos_Bin, "reaction", "accum_level[0][0][0]>7");
-    //draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_Cos_NBin, FHCNumuCC0Pi_Cos_Bin, "reaction", "accum_level[0][0][0]>7", "", "", false, POT_Weight, false);    
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
+    draw.SetTitle("CC0Pi-Np-0#gamma FGD1");
+    draw.SetTitleX("Proton candidate Cos#thete");
+    draw.Draw(exp, "sel_prot_dir_vertex[0][2]", 12., Blarb, "reaction", "accum_level[0][0][2]>8");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
+
     draw.SetLegendPos("tl");
-    draw.SetTitle("CC0Pi-0p FGD1");
-    draw.SetTitleX("#mu^{-} candidate cos#theta");
-    draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_0p_Cos_NBin, FHCNumuCC0Pi_0p_Cos_Bin, "reaction", "accum_level[0][0][0]>8"); 
-    //draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_0p_Cos_NBin, FHCNumuCC0Pi_0p_Cos_Bin, "reaction", "accum_level[0][0][0]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
+    draw.SetTitle("CC0#pi-Np-0#gamma FGD1");
+    draw.SetTitleX("Proton candidate Cos#{thete}");
+    draw.Draw(exp, "sel_prot_dir_vertex[0][2]", 12, Blarb, "all", "accum_level[0][0][2]>8");
+    c1->Print("ProtonSelecPlotsNew.pdf", "pdf");
     
-    draw.SetLegendPos("tl");
-    draw.SetTitle("CC0Pi-Np FGD1");
-    draw.SetTitleX("#mu^{-} candidate cos#theta");
-    draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_Np_Cos_NBin, FHCNumuCC0Pi_Np_Cos_Bin, "reaction", "accum_level[0][0][1]>8"); 
-    //draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_Np_Cos_NBin, FHCNumuCC0Pi_Np_Cos_Bin, "reaction", "accum_level[0][0][1]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    draw.SetLegendPos("tl");
-    draw.SetTitle("CC0Pi-0p FGD2");
-    draw.SetTitleX("#mu^{-} candidate cos#theta");
-    draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_0p_Cos_NBin, FHCNumuCC0Pi_0p_Cos_Bin, "fgd2reaction", "accum_level[0][1][0]>8");
-    //draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_0p_Cos_NBin, FHCNumuCC0Pi_0p_Cos_Bin, "fgd2reaction", "accum_level[0][1][0]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    draw.SetLegendPos("tl");
-    draw.SetTitle("CC0Pi-Np FGD2");
-    draw.SetTitleX("#mu^{-} candidate cos#theta");
-    draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_Np_Cos_NBin, FHCNumuCC0Pi_Np_Cos_Bin, "fgd2reaction", "accum_level[0][1][1]>8"); 
-    //draw.Draw(data, mc, "selmu_costheta",FHCNumuCC0Pi_Np_Cos_NBin, FHCNumuCC0Pi_Np_Cos_Bin, "fgd2reaction", "accum_level[0][1][1]>8", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    
-    
-    draw.SetTitle("FGD1");
-    draw.SetTitleX("Momentum of FGD proton candidate");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "FGDProtMom",40., 0., 2500. ,"reaction", "accum_level[0][0][0]>7"); 
-    //draw.Draw(data, mc, "FGDProtMom",40., 0., 2500. ,"reaction", "accum_level[0][0][0]>7", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-        
-    draw.SetTitle("FGD1");
-    draw.SetTitleX("Momentum of TPC proton candidate");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "TPCProtMom",40., 0., 2500. ,"reaction", "accum_level[0][0][0]>7"); 
-    //draw.Draw(data, mc, "TPCProtMom",40., 0., 2500. ,"reaction", "accum_level[0][0][0]>7", "", "", false, POT_Weight, false); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    
-    draw.SetTitle("FGD2");
-    draw.SetTitleX("Momentum of FGD proton candidate");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "FGDProtMom",40., 0., 2500. ,"fgd2reaction", "accum_level[0][1][0]>7"); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-        
-    draw.SetTitle("FGD2");
-    draw.SetTitleX("Momentum of TPC proton candidate");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "TPCProtMom",40., 0., 2500. ,"fgd2reaction", "accum_level[0][1][0]>7"); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    
-    
-    draw.SetTitle("FGD1");
-    draw.SetTitleX("Number of recosntucted FGD protons");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "nProtonsFGD",5., 0., 5. ,"reaction", "accum_level[0][0][0]>7"); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-        
-    draw.SetTitle("FGD1");
-    draw.SetTitleX("Number of recosntucted TPC protons");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "nProtonsTPC",5., 0., 5. ,"reaction", "accum_level[0][0][0]>7"); ; 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    
-    draw.SetTitle("FGD2");
-    draw.SetTitleX("Number of recosntucted FGD protons");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "nProtonsFGD",5., 0., 5. ,"fgd2reaction", "accum_level[0][1][0]>7"); 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-        
-    draw.SetTitle("FGD2");
-    draw.SetTitleX("Number of recosntucted TPC protons");
-    draw.SetLegendPos("tr");
-    draw.Draw(data, mc, "nProtonsTPC",5., 0., 5. ,"fgd2reaction", "accum_level[0][1][0]>7"); ; 
-    c1->Print("ProtonSelecPlots.pdf", "pdf");
-    
-    c1->Print("ProtonSelecPlots.pdf]", "pdf");
+    c1->Print("ProtonSelecPlotsNew.pdf]", "pdf");
     delete c1;
 }
